e6c4e6889b251fead921dbbda5058bdffee44165
joshuakaufman3827 <148819008+joshuakaufman3827@users.noreply.github.com>
2026-09-12 12:07:09 -0500

    rewritten update

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,40 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+  const [sellers, setSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTopSellers = async () => {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+        );
+        setSellers(response.data);
+      } catch (error) {
+        console.error("Error fetching top sellers:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTopSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
         <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Top Sellers</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
+
+          <div className="col-lg-12 text-center">
+            <h2>Top Sellers</h2>
+            <div className="small-border bg-color-2"></div>
           </div>
+
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading
+                ? new Array(12).fill(0).map((_, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <div className="skeleton-avatar"></div>
+                      </div>
+                      <div className="author_list_info">
+                        <div className="skeleton-text title"></div>
+                        <div className="skeleton-text code"></div>
+                      </div>
+                    </li>
+                  ))
+                : sellers.map((seller) => (
+                    <li key={seller.authorId}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${seller.authorId}`}>
+                          <img
+                            className="lazy pp-author"
+                            src={seller.authorImage}
+                            alt={seller.authorName}
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+
+                      <div className="author_list_info">
+                        <Link to={`/author/${seller.authorId}`}>
+                          {seller.authorName}
+                        </Link>
+                        <span>{seller.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
             </ol>
           </div>
+
         </div>
       </div>
     </section>
@@ -42,3 +78,4 @@ const TopSellers = () => {
 };
 
 export default TopSellers;
+
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,40 +1,61 @@
-import React from "react";
-import AuthorBanner from "../images/author_banner.jpg";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import authors from "../data/authors.json";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
 
 const Author = () => {
+  const { id } = useParams();
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    const found = authors.find(
+      (a) => a.authorId.toString() === id.toString()
+    );
+    setAuthor(found || null);
+  }, [id]);
+
+  if (!author)
+    return (
+      <div className="container text-center">
+        <h2>Author profile not found.</h2>
+        <p>Target Author ID: {id}</p>
+        <Link to="/" className="btn-main">Back to Home</Link>
+      </div>
+    );
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
 
         <section
           id="profile_banner"
-          aria-label="section"
           className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
+          style={{ background: `url(${author.authorBanner}) top` }}
         ></section>
 
         <section aria-label="section">
           <div className="container">
             <div className="row">
+
               <div className="col-md-12">
                 <div className="d_profile de-flex">
+
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
-
+                      <img src={author.authorImage} alt={author.authorName} />
                       <i className="fa fa-check"></i>
+
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                          {author.authorName}
+                          <span className="profile_username">
+                            @{author.authorUsername}
                           </span>
+
+                          <span id="wallet" className="profile_wallet">
+                            {author.authorWallet}
+                          </span>
+
                           <button id="btn_copy" title="Copy Text">
                             Copy
                           </button>
@@ -42,28 +63,34 @@ const Author = () => {
                       </div>
                     </div>
                   </div>
+
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      <div className="profile_follower">
+                        {author.authorFollowers} followers
+                      </div>
+                      <Link to="#" className="btn-main">Follow</Link>
                     </div>
                   </div>
+
                 </div>
               </div>
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems items={author.nfts} />
                 </div>
               </div>
+
             </div>
           </div>
         </section>
+
       </div>
     </div>
   );
 };
 
 export default Author;
+
+
