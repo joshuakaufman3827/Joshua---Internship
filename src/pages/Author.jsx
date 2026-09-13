fb6aeac0964b2c9f66d3203dd5d0262da5ed017d
joshuakaufman3827 <148819008+joshuakaufman3827@users.noreply.github.com>
2026-09-13 14:14:48 -0500

    updated author.jsx

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,37 +1,88 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import authors from "../data/authors.json";
 import AuthorItems from "../components/author/AuthorItems";
+import AuthorBanner from "../images/author_banner.jpg";
 
 const Author = () => {
   const { id } = useParams();
   const [author, setAuthor] = useState(null);
+    address: "",
+    authorID: 0,
+    authorImage: "",
+    authorName: "",
+    followers: 0,
+    tag: "",
+});
+const [authorItems, setAuthorItems] = useState([])
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(false);
 
-  useEffect(() => {
-    const found = authors.find(
-      (a) => a.authorId.toString() === id.toString()
+console.log(id);
+//useEffect(() => {
+//const found = authors.find(a => a.authorId.toString() === id.toString());
+//setAuthor(found || null);
+//}, [id]);
+
+async function fetchAuthor() {
+  try {
+    const response = await fetch(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?
+      author=${id}`,
     );
-    setAuthor(found || null);
+    
+    const author = await response.json();
 
+    console.log(author);
+    const {
+     address,
+     authorID,
+     authorImage,
+     authorName,
+     followers,
+     tag,
+     nftCollection,
+    } = author;
+
+    setAuthor ({
+      address,
+      authorID,
+      authorImage,
+      authorName,
+      followers,
+      tag,
+    });
+
+    setAuthorItems(nftCollection);
+  } catch (error) {
+   setError(true);
+   console.log("Error fetching author:", error);
+  } finally {
+    setLoading(false);
+    }
+  }
+
+   useEffect(() => {
+    fetchAuthor();
   }, [id]);
 
-  if (!author)
+  if (error)
     return (
-      <div className="container text-center">
+      <div className="container text=center author-error">
         <h2>Author profile not found.</h2>
         <p>Target Author ID: {id}</p>
-        <Link to="/" className="btn-main">Back to Home</Link>
+        <Link to="/" className="btn-main">
+        Back to Home
+        </Link>
       </div>
     );
 
-  return (
+    return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-
         <section
           id="profile_banner"
           className="text-light"
-          style={{ background: `url(${author.authorBanner}) top` }}
+          style={{ background: `url(${AuthorBanner}) top` }}
         ></section>
 
         <section aria-label="section">
@@ -39,7 +90,35 @@ const Author = () => {
             <div className="row">
 
               <div className="col-md-12">
+              {loading ? (
                 <div className="d_profile de-flex">
+                 {/* Replace with skeleton Loading State Later on */}
+                <div className="de-flex-col">
+                  <div className="profile_avatar">
+                   Loading...
+                   <i className="fa fa-check"></i>
+                   <h4>
+                    {author.authorName}
+                    <span className="profile_username">Loading...</span>
+
+                    <span id="wallet" className="profile_wallet">
+                    Loading...
+                    </span>
+
+                    <button id="btn_copy" title="Copy Text">
+                     Loading...
+                    </button>
+                   </h4>
+                  </div>
+                 </div>
+                </div>
+              ) : (
+                <div className="d_profile de-flex">
+                  <div className="de-flex-col">
+                    <div className="profile_avatar">
+                      <img src={author.authorImage} alt={author.authorName} />
+                      <i className="fa fa-check"></i>
+            
 
                   <div className="de-flex-col">
                     <div className="profile_avatar">
