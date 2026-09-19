fa86f9f12722b98ee70f72ade7eaaabc5fb6d8ee
joshuakaufman3827 <148819008+joshuakaufman3827@users.noreply.github.com>
2026-09-19 12:01:38 -0500

    Updaing author.jsx

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,38 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const AuthorItems = ({ items }) => {
-  if (!items || items.length === 0) {
+const AuthorItems = ({ items, loading }) => {
+  if (!loading && (!items || items.length === 0)) {
     return <p className="text-center">No items found for this author.</p>;
   }
 
   return (
     <div className="row">
-      {items.map((item) => (
-        <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={item.id}>
-          <div className="nft__item">
-            <div className="nft__item_wrap">
-              <Link to={`/item/${item.id}`}>
-                <img
-                  src={item.image}
-                  className="lazy nft__item_preview"
-                  alt={item.title}
-                />
-              </Link>
-            </div>
-
-            <div className="nft__item_info">
-              <Link to={`/item/${item.id}`}>
-                <h4>{item.title}</h4>
-              </Link>
-
-              <div className="nft__item_price">
-                {item.price} ETH
+      {loading ? (
+        <>
+          {new Array(9).fill(0).map((_, index) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+              <div className="nft__item">
+                <div className="nft__item_wrap">Loading...</div>
+                <div className="nft__item_info">
+                  Loading...
+                  <div className="nft__item_price">Loading price...</div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      ))}
+          ))}
+        </>
+      ) : (
+        <>
+          {items.map((item) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={item.id}>
+              <div className="nft__item">
+                <div className="nft__item_wrap">
+                  <Link to={`/item/${item.id}`}>
+                    <img
+                      src={item.nftImage}
+                      className="lazy nft__item_preview"
+                      alt={item.title}
+                    />
+                  </Link>
+                </div>
+                <div className="nft__item_info">
+                  <Link to={`/item/${item.id}`}>{item.title}</Link>
+                  <div className="nft__item_price">{item.price} ETH</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -5,172 +5,191 @@ import AuthorBanner from "../images/author_banner.jpg";
 
 const Author = () => {
   const { id } = useParams();
-  const [author, setAuthor] = useState(null);
+  const [author, setAuthor] = useState({
     address: "",
     authorID: 0,
     authorImage: "",
     authorName: "",
     followers: 0,
     tag: "",
-});
-const [authorItems, setAuthorItems] = useState([])
-const [loading, setLoading] = useState(true);
-const [error, setError] = useState(false);
+  });
+  const [authorItems, setAuthorItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
 console.log(id);
-//useEffect(() => {
-//const found = authors.find(a => a.authorId.toString() === id.toString());
-//setAuthor(found || null);
-//}, [id]);
+// useEffect(() => {
+//   const found = authors.find((a) => a.authorId.toString() === id.toString());
+//   setAuthor(found || null);
+// }, [id]);
 
 async function fetchAuthor() {
   try {
-    const response = await fetch(
-      "https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?
-      author=${id}`,
-    );
-    
-    const author = await response.json();
+  const response = await fetch(
+    `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?=${id}`,
+  );
 
-    console.log(author);
-    const {
-     address,
-     authorID,
-     authorImage,
-     authorName,
-     followers,
-     tag,
-     nftCollection,
-    } = author;
+  const author = await response.json();
 
-    setAuthor ({
-      address,
-      authorID,
-      authorImage,
-      authorName,
-      followers,
-      tag,
-    });
+  console.log(author);
+  const { 
+    address,
+    authorID,
+    authorImage,
+    authorName,
+    followers,
+    tag,
+    nftCollection,
+  } = author;
 
-    setAuthorItems(nftCollection);
-  } catch (error) {
-   setError(true);
-   console.log("Error fetching author:", error);
-  } finally {
-    setLoading(false);
-    }
-  }
+  setAuthor({
+    address,
+    authorID,
+    authorImage,
+    authorName,
+    followers,
+    tag,
+  });
 
-   useEffect(() => {
-    fetchAuthor();
-  }, [id]);
+  setAuthorItems(nftCollection);
+} catch (error) {
+  setError (true);
+  console.log("Error while fetching author: ", error);
+} finally {
+  setLoading(false);
+ }
+}
 
-  if (error)
-    return (
-      <div className="container text=center author-error">
-        <h2>Author profile not found.</h2>
-        <p>Target Author ID: {id}</p>
-        <Link to="/" className="btn-main">
-        Back to Home
-        </Link>
-      </div>
-    );
+ useEffect(() => {
+   fetchAuthor();
+   }, [id]);
 
-    return (
+ if (error)
+   return (
+   <div className="container text-center author-error">
+    <h2>Author profile not found.</h2>
+    <p>Target Author ID: {id}</p>
+    <Link to="/" className="btn-main">
+      Back to Home
+      </Link>
+   </div>
+  );
+
+  return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-        <section
-          id="profile_banner"
-          className="text-light"
-          style={{ background: `url(${AuthorBanner}) top` }}
-        ></section>
-
-        <section aria-label="section">
-          <div className="container">
-            <div className="row">
-
-              <div className="col-md-12">
+       <section
+        id="profile_banner"
+        className="text-light"
+        style={{ background: 'url($(AuthorBanner}} top` }}
+      ></section>
+      
+      <section aria-label="section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
               {loading ? (
                 <div className="d_profile de-flex">
-                 {/* Replace with skeleton Loading State Later on */}
-                <div className="de-flex-col">
-                  <div className="profile_avatar">
-                   Loading...
-                   <i className="fa fa-check"></i>
-                   <h4>
-                    {author.authorName}
-                    <span className="profile_username">Loading...</span>
-
-                    <span id="wallet" className="profile_wallet">
+                  <div className="de-flex-col">
+                   <div className="profile_avatar">
                     Loading...
-                    </span>
+                    <i className="fa fa-check"></i>
+                    <div className="profile_name">
+                      <h4>
+                        {author.authorName}
+                        <span className="profile_username">Loading...</span>
 
-                    <button id="btn_copy" title="Copy Text">
-                     Loading...
-                    </button>
-                   </h4>
-                  </div>
-                 </div>
-                </div>
-              ) : (
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={author.authorImage} alt={author.authorName} />
-                      <i className="fa fa-check"></i>
-            
-
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={author.authorImage} alt={author.authorName} />
-                      <i className="fa fa-check"></i>
-
-                      <div className="profile_name">
-                        <h4>
-                          {author.authorName}
-                          <span className="profile_username">
-                            @{author.authorUsername}
+                        <span id="wallet" className="profile_wallet">
+                          Loading...
                           </span>
 
+                          <button id ="btn_copy" title="Copy Text">
+                          Loading...
+                          </button>
+                         </h4>
+                       </div>
+                      </div>
+                    </div>
+
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <img src={author.authorImage} alt={author.authorName} />
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name"></div>
+                        <h4>
+                          {author.authorName}
+                          <span className="profile_username">Loading...</span>
+
                           <span id="wallet" className="profile_wallet">
-                            {author.authorWallet}
+                            Loading...
                           </span>
 
                           <button id="btn_copy" title="Copy Text">
-                            Copy
+                            Loading...
                           </button>
                         </h4>
                       </div>
                     </div>
                   </div>
 
-                  <div className="profile_follow de-flex">
+                  <div className="profile_follow deflex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">
-                        {author.authorFollowers} followers
+                      <div className="profile_followers">Loading...</div>
+                      <Link to ="#" className="btn-main">
+                        Follow
+                      </Link>
                       </div>
-                      <Link to="#" className="btn-main">Follow</Link>
                     </div>
                   </div>
+               )}
 
-                </div>
-              </div>
-
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems items={author.nfts} />
-                </div>
-              </div>
-
+                          }></Loading>
+                        </spam>
+                      </h4>
+              )}
             </div>
           </div>
-        </section>
-
+        </div>
+      </section>
       </div>
-    </div>
-  );
-};
 
+  )
+// if (!author)
+//return (
+//   <div 
+// className="container text-center")
+//   style={{
+//     minHeight: "100vh",
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     flexDirection: "column",
+//   }};
+// );
+
+return {
+  <div id="wrapper">
+    <div className="no-bottom no-top" id="content">
+      <section
+        id="profile_banner"
+        className="text-light"
+        style={{ background: `url(${AuthorBanner}) top` }}
+      ></section>
+
+return (
+<section aria-label="section">
+  <div className="container">
+    <div className="row"> 
+      <div ClassName="col-md-12">
+        <div className="d_profile de-flex">
+          <div className="de-flex-col"> 
+     style={{ background: `url(${AuthorBanner}) top` }}
+    ></section>
+
+
+
+                    
 export default Author;
 
 
